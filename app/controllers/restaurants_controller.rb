7f284b6fbf4d17c1e87517edae238710afe16f37
Stefan Hybschmann <stefan.hybschmann@gmail.com>
2020-07-08 17:04:14 +0200

    Finished the challenge with pair coding with Millan Garcia - the entire program worked

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,16 @@
+class RestaurantsController < ApplicationController
+  def name:string
+  end
+
+  def address:string
+  end
+
+  def phone_number:string
+  end
+
+  def category:string
+  end
+
+  def reviews:text
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'restaurants/name:string'
+  get 'restaurants/address:string'
+  get 'restaurants/phone_number:string'
+  get 'restaurants/category:string'
+  get 'restaurants/reviews:text'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
